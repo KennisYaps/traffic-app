c1cdd6041baa1cc3c8f3e644fb70a8bd41baf038
yappps <yapyuanxin@gmail.com>
2018-05-01 15:05:40 +0800

    Added  components to <Main/>

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ERPRates from "../ERP_Rates/ERP_Rates";
+import TrafficImage from "../Traffic_Image/Traffic_Image";
+import TrafficIncidents from "../Traffic_Incidents/Traffic_Incidents";
+import EstTravelTimes from "../EstTravelTimes/EstTravelTimes";
+import TrafficSpeedBands from "../TrafficSpeedBands/TrafficSpeedBands";
+import VMS from "../VMS/VMS";
+const Main = () => (
+  <div>
+    <ERPRates />
+    <TrafficIncidents />
+    <TrafficImage />
+    <EstTravelTimes />
+    <TrafficSpeedBands />
+    <VMS />
+  </div>
+);
+
+export default Main;
