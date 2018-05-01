8394239c576afcd76a99f53d32ba82e6c06ce586
yappps <yapyuanxin@gmail.com>
2018-05-01 15:06:49 +0800

    Added VMS component

---
+++ src/Components/VMS/VMS.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import fetchAPI from "../../utils/fetchAPI";
+class VMS extends Component {
+  constructor() {
+    super();
+    this.state = {
+      VMSDatas: []
+    };
+  }
+
+  componentDidMount() {
+    fetchAPI("VMS").then(response => {
+      if (response.ok) {
+        response.json().then(json => {
+          this.setState({
+            VMSDatas: json.value
+          });
+          console.log(this.state.VMSDatas);
+        });
+      } else {
+        console.log("response is not okay");
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        Returns traffic advisories (via variable message services) concerning
+        current traffic conditions that are displayed on EMAS signboards along
+        expressways and arterial roads.
+      </div>
+    );
+  }
+}
+
+export default VMS;
