9a95d3d69128f74e081cd8df9dbb288c52fcc346
yappps <yapyuanxin@gmail.com>
2018-05-02 20:41:02 +0800

    added <MapWithAMarkerClusterer /> and manage to setInterval successfully

--- src/Components/Traffic_Image/Traffic_Image.js
+++ src/Components/Traffic_Image/Traffic_Image.js
@@ -1,14 +1,47 @@
 import React, { Component } from "react";
 import fetchAPI from "../../utils/fetchAPI";
+import MapWithAMarkerClusterer from "../MapWithAMarkerClusterer";
 class TrafficImage extends Component {
   constructor() {
     super();
     this.state = {
       trafficImageDatas: []
     };
+    this.fetchData = this.fetchData.bind(this);
   }
 
+  render() {
+    const displayTrafficImageLinks = this.state.trafficImageDatas.map(
+      (image, idx) => {
+        return (
+          <li key={idx}>
+            {image.CameraID}&nbsp;&nbsp;&nbsp;&nbsp;{image.ImageLink}
+          </li>
+        );
+      }
+    );
+
+    return (
+      <div>
+        <h1>
+          Traffic Image of live traffic conditions along expressways and
+          Woodlands & Tuas Checkpoints
+        </h1>
+        <div>
+          <MapWithAMarkerClusterer markers={this.state.trafficImageDatas} />
+        </div>
+        {/* <div>{displayTrafficImageLinks}</div> */}
+      </div>
+    );
+  }
   componentDidMount() {
+    this.fetchData();
+    this.interval = setInterval(this.fetchData, 240000); // 4 mins
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  fetchData() {
     fetchAPI("Traffic-Images").then(response => {
       if (response.ok) {
         response.json().then(json => {
@@ -21,10 +54,6 @@ class TrafficImage extends Component {
         console.log("response is not okay");
       }
     });
-  }
-  render() {
-    return <div>Traffic Image of live traffic conditions along expressways
-    and Woodlands & Tuas Checkpoints</div>;
   }
 }
 
--- src/Components/Traffic_Incidents/Traffic_Incidents.js
+++ src/Components/Traffic_Incidents/Traffic_Incidents.js
@@ -1,31 +1,63 @@
 import React, { Component } from "react";
 import fetchAPI from "../../utils/fetchAPI";
+import MapWithAMarkerClusterer from "../MapWithAMarkerClusterer";
+import { Input } from "semantic-ui-react";
+
 class TrafficIncidents extends Component {
   constructor() {
     super();
     this.state = {
       TrafficIncidentsDatas: []
     };
+    this.fetchData = this.fetchData.bind(this);
   }
+  // componentWillMount() {
+  //   this.setState({ TrafficIncidentsDatas: [] });
+  // }
 
-  componentDidMount() {
-    fetchAPI("TrafficIncidents").then(
-      response => {
-        if (response.ok) {
-          response.json().then(json => {
-            this.setState({
-              TrafficIncidentsDatas: json.value
-            });
-            console.log(this.state.TrafficIncidentsDatas);
-          });
-        } else {
-          console.log("response is not okay");
-        }
+  render() {
+    const displayIncidentsMessages = this.state.TrafficIncidentsDatas.map(
+      (incident, idx) => {
+        return (
+          <li key={idx}>
+            {incident.Type}&nbsp;&nbsp;&nbsp;&nbsp;{incident.Message}
+          </li>
+        );
       }
     );
+    return (
+      <div>
+        <h1>Traffic Incidents</h1>
+        <Input fluid loading={false} icon="search" placeholder="Search..." />
+        {/* {displayIncidentsMessages} */}
+        <h4>The Incident Map</h4>
+        <div>
+          <MapWithAMarkerClusterer markers={this.state.TrafficIncidentsDatas} />
+        </div>
+      </div>
+    );
   }
-  render() {
-    return <div>Traffic Incidents</div>;
+  componentDidMount() {
+    this.fetchData();
+    this.fetchReverseGeoCodedStreetNames =
+    this.interval = setInterval(this.fetchData, 120000); // 2 mins
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  fetchData() {
+    fetchAPI("TrafficIncidents").then(response => {
+      if (response.ok) {
+        response.json().then(json => {
+          this.setState({
+            TrafficIncidentsDatas: json.value
+          });
+        });
+      } else {
+        console.log("response is not okay");
+      }
+    });
   }
 }
 
