ebc7170256f686db2868fcf2f29a2cc0a27f3b50
yappps <yapyuanxin@gmail.com>
2018-05-03 21:54:35 +0800

    commented out all components except TrafficIncidents

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,18 +1,18 @@
 import React from "react";
-import ERPRates from "../ERP_Rates/ERP_Rates";
-import TrafficImage from "../Traffic_Image/Traffic_Image";
+// import ERPRates from "../ERP_Rates/ERP_Rates";
+// import TrafficImage from "../Traffic_Image/Traffic_Image";
 import TrafficIncidents from "../Traffic_Incidents/Traffic_Incidents";
-import EstTravelTimes from "../EstTravelTimes/EstTravelTimes";
-import TrafficSpeedBands from "../TrafficSpeedBands/TrafficSpeedBands";
-import VMS from "../VMS/VMS";
+// import EstTravelTimes from "../EstTravelTimes/EstTravelTimes";
+// import TrafficSpeedBands from "../TrafficSpeedBands/TrafficSpeedBands";
+// import VMS from "../VMS/VMS";
 const Main = () => (
   <div>
-    <ERPRates />
+    {/* <ERPRates /> */}
     <TrafficIncidents />
-    <TrafficImage />
-    <EstTravelTimes />
-    <TrafficSpeedBands />
-    <VMS />
+    {/* <TrafficImage /> */}
+    {/* <EstTravelTimes /> */}
+    {/* <TrafficSpeedBands /> */}
+    {/* <VMS /> */}
   </div>
 );
 
